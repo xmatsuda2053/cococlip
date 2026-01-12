2adf35906b58bc673f21e9d6e6fbf6efbc41ef49
Kazuya Matsuda <alsterwasser.sg@gmail.com>
2026-01-12 16:13:15 +0900

    feat: ccc-tableコンポーネントのスタイルと機能の改善
    - shared.lit.scss: スクロールバーのスタイルを調整し、不要なスタイルをコメントアウト
    - ccc-app.lit.scss: グリッドのギャップを0に設定し、ccc-mainに最小高さとオーバーフローを追加
    - ccc-table.lit.scss: 新規作成、テーブルのスタイルを定義
    - ccc-table.ts: テーブルの構造を変更し、クリックしたセルの値をクリップボードにコピーする機能を追加

--- src/components/ccc-table/ccc-table.ts
+++ src/components/ccc-table/ccc-table.ts
@@ -10,12 +10,14 @@ import { customElement, state, property, query } from "lit/decorators.js";
 import { repeat } from "lit/directives/repeat.js";
 import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js";
 
+import { toastSuccess } from "@service/utils";
 import { db } from "@service/db";
 import type { FileData } from "@/models/FileData";
 import type { FileMeta } from "@/models/FileMeta";
 
 import "@shoelace-style/shoelace/dist/themes/light.css";
 
+import sharedStyles from "@assets/styles/shared.lit.scss?inline";
 import styles from "./ccc-table.lit.scss?inline";
 
 setBasePath("/");
@@ -54,9 +56,14 @@ export class CccTable extends LitElement {
    * @static
    * @memberof CccTable
    */
-  static styles = css`
-    ${unsafeCSS(styles)}
-  `;
+  static styles = [
+    css`
+      ${unsafeCSS(sharedStyles)}
+    `,
+    css`
+      ${unsafeCSS(styles)}
+    `,
+  ];
 
   /**
    * Creates an instance of CccTable.
@@ -121,23 +128,45 @@ export class CccTable extends LitElement {
       return html`<p>データがありません</p>`;
     }
 
-    return html`<table>
-      <tbody>
-        <tr>
-          ${this._fileMeta?.headers?.map((f) => html`<td>${f}</td>`)}
-        </tr>
-      </tbody>
-      <tbody>
-        ${repeat(
-          this._fileData,
-          (d) => d.id,
-          (d) => html` <tr>
-            ${this._fileMeta?.headers?.map(
-              (h: string) => html` <td>${d.data[h]}</td>`
-            )}
-          </tr>`
-        )}
-      </tbody>
-    </table>`;
+    return html`<div id="root" class="scrollable">
+      <table>
+        <thead>
+          <tr>
+            ${this._fileMeta?.headers?.map((f) => html`<th>${f}</th>`)}
+          </tr>
+        </thead>
+        <tbody>
+          ${repeat(
+            this._fileData,
+            (d) => d.id,
+            (d) => html` <tr>
+              ${this._fileMeta?.headers?.map(
+                (h: string) =>
+                  html` <td @click=${this._handleClickTd}>${d.data[h]}</td>`
+              )}
+            </tr>`
+          )}
+        </tbody>
+      </table>
+    </div>`;
+  }
+
+  /**
+   * クリックしたセルの値をクリップボードにコピーする。
+   *
+   * @private
+   * @param {Event} e
+   * @memberof CccTable
+   */
+  private async _handleClickTd(e: Event) {
+    e.preventDefault();
+    const target = e.target as HTMLElement;
+    if (!target) {
+      return;
+    }
+
+    await navigator.clipboard.writeText(target.innerText);
+
+    toastSuccess("コピー完了", "クリップボードにコピーしました");
   }
 }
